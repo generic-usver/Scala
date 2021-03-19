edbcad6f4d56e139d0f3a3bc0507f8969d83dc1a
usver <generic.usver@gmail.com>
2021-03-19 09:32:40 +0200

    Scala objects == "static" companion objects. Singleton Objects instead of STATIC

---
+++ src/main/scala/lectures/part2oop/Objects.scala
@@ -0,0 +1,34 @@
+package com.usver
+package lectures.part2oop
+
+object Objects extends App {
+
+  // SCALA does not have STATIC concept
+  // Equivalent:
+  object Person {
+
+    val N_EYES = 2
+    def canFly: Boolean = false
+
+    // from == FACTORY METHOD
+    // sole purpose = make new Instances
+    def from(mother: Person, father: Person): Person = new Person("bobby")
+
+    def apply(mother: Person, father: Person) = from(mother, father)
+  }
+  class Person(val name: String) {
+    // instance-level functionality
+  }
+  // COMPANIONS == Object+class together
+
+  println(Person.N_EYES)
+  println(Person.canFly)
+
+  // Scala object == SINGLETON
+  val mary = new Person("mary")
+  val john = new Person("john")
+
+  println(mary == john)
+  val person = new Person("person") // new instance
+  val bobby = Person(mary, john)
+}

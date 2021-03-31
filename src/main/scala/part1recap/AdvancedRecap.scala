b62d2a037b04a399864ec424a24565f98acb01bf
usver <generic.usver@gmail.com>
2021-03-31 21:29:06 +0300

    Partial Scala function orElse == complimenting, extending value coverage

--- src/main/scala/part1recap/AdvancedRecap.scala
+++ src/main/scala/part1recap/AdvancedRecap.scala
@@ -30,4 +30,17 @@ object AdvancedRecap extends App {
   val lifted = partialFunction.lift  // partial becomes TOTAL function Int => Option[Int]
   lifted(2)   // Some(64)
   lifted(20)  // None
+
+  // orElse
+  val partialFunctionChain = partialFunction.orElse[Int, Int] {
+    case 60 => 9000
+  }
+
+  // orElse gives extra values to the "partially covered" values.
+  // It can "compliment" or "extend" the original incomplete value set
+
+  partialFunctionChain(5)   // 999 from the original
+  partialFunctionChain(60)  // 9000 from "orElse" add-on
+  //partialFunctionChain(456) // thows matchError
+  
 }

904c91d8b0b1213b0bf66cec0e19582488e56504
generic-usver <generic.usver@gmail.com>
2021-04-03 23:42:59 +0300

    Throwing Exception restarts Actor. preRestart(throw), postRestart(throw) handle those

--- src/main/scala/part4faulttolerance/ActorLifecycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifecycle.scala
@@ -7,9 +7,15 @@ object ActorLifecycle extends App {
   val actorSystem = ActorSystem("actorSystem")
   val parent = actorSystem.actorOf(Props[SomeActor],"parent")
 
+  // Triggering start/stop event listeners
   parent ! StartChild("child")
   Thread.sleep(1000)
   parent ! PoisonPill
+
+  // Restart features
+
+  val parentWithChild = actorSystem.actorOf(Props[SupervisingParent],"parentWithChild")
+  parentWithChild ! FailChild
 }
 case class StartChild(name: String)
 
@@ -21,5 +27,35 @@ class SomeActor extends Actor with ActorLogging {
   lazy val name = context.self.path.name
   override def receive: Receive = {
     case StartChild(name) => context.actorOf(Props[SomeActor], name)
+  }
+}
+
+object FailObject
+object FailChild
+
+class SupervisingParent extends Actor with ActorLogging {
+  val child = context.actorOf(Props[SupervisedChild], "supervisedChild")
+  override def receive: Receive = {
+    case FailChild => child ! FailObject
+  }
+}
+class SupervisedChild extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case FailObject => {
+      log.warning("Child will fail now")
+      throw new RuntimeException("I failed")
+    };
+  }
+
+  override def preStart(): Unit = log.info("supervised child starting")
+  override def postStop(): Unit = log.info("supervised child stopped")
+
+  // Before replacing the Actor object
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    log.warning(s"Supervised actor restarting because ${reason.getMessage}, message: $message")
+  }
+  // After replacing the Actor object
+  override def postRestart(reason: Throwable): Unit = {
+    log.warning(s"Supervised child done restarting because: ${reason.getMessage}")
   }
 }
